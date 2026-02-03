5f7024d67d5da7db2724e39e386f0ab69f857589
innocentioz <troublezhahahaha@gmail.com>
2026-02-03 12:49:48 +0300

    feat: edit dto files for book

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,12 +1,15 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class UpdateBookDto {
   @IsString()
+  @MaxLength(64, { message: "Название книги должно быть не больше 64 символов" })
   title: string;
 
   @IsString()
+  @MaxLength(64, { message: "Имя автора должно быть не больше 64 символов" })
   author: string;
 
   @IsString()
+  @MaxLength(64, { message: "Название издательства должно быть не больше 64 символов" })
   publisher: string;
 }
