b9be582400bd31426703125d022d1d53e7693ebf
innocentioz <troublezhahahaha@gmail.com>
2026-02-03 12:48:03 +0300

    feat: base crud book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BookController {
@@ -18,6 +19,16 @@ export class BookController {
 
   @Get("/get/:slug")
   async getAllBookByTitle(@Param("slug") title: string) {
-    return this.bookService.getBookByTitle(title)
+    return this.bookService.searchBooksByTitle(title)
+  }
+
+  @Patch(":id/update")
+  async updateBook(@Param("id") book_id: number, @Body() dto: UpdateBookDto) {
+    return this.bookService.updateBook(book_id, dto);
+  }
+
+  @Delete(":id/delete")
+  async deleteBook(@Param("id") book_id: number) {
+    return this.bookService.deleteBook(book_id);
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,13 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
 
 @Injectable()
 export class BookService {
     constructor(private readonly prismaService: PrismaService) {}
 
-    async getBookByTitle(title: string) {
-        return this.prismaService.book.findMany({
+    async getBookById(book_id: number) {
+        const book = await this.prismaService.book.findUnique({
+            where: {
+                id: book_id
+            }
+        })
+        if (!book) {
+            throw new NotFoundException({
+                message: "Книга не найдена"
+            })
+        }
+        return book;
+    }
+
+    async deleteBook(book_id: number) {
+        await this.getBookById(book_id)
+        return await this.prismaService.book.delete({
+            where: {
+                id: book_id
+            }
+        })
+    }
+
+    async updateBook(book_id: number, dto: UpdateBookDto) {
+        await this.getBookById(book_id);
+        const { title, author, publisher } = dto;
+        if (!title && !author && !publisher) {
+            throw new BadRequestException({
+                message: "Должно быть заполнено хотя-бы одно поле"
+            })
+        }
+        try {
+            return await this.prismaService.book.update({
+                where: { id: book_id },
+                data: {
+                    title,
+                    author,
+                    publisher        
+                }
+            })
+        } catch (error) {
+            console.error(error);
+            throw new BadRequestException({
+                message: `Произошла ошибка при обновлении: ${error}`
+            })
+        }
+    }
+
+    async searchBooksByTitle(title: string) {
+        const book = this.prismaService.book.findMany({
             where: {
                 title: {
                     contains: title,
@@ -15,27 +65,50 @@ export class BookService {
                 }
             }
         })
+        if (!book) {
+            throw new NotFoundException({
+                message: "Книга не найдена"
+            })
+        }
+        return book;
     }
 
     async getAllBooks() {
-        return this.prismaService.book.findMany({
+        const books = this.prismaService.book.findMany({
             select: {
                 title: true,
                 author: true,
                 publisher: true,
             }
         })
+        if (!books) {
+            throw new NotFoundException({
+                message: "Книги не найдены"
+            })
+        }
+        return books;
     }
 
     async createBook(dto: CreateBookDto) {
         const { title, author, publisher } = dto;
-
-        return this.prismaService.book.create({
-            data: {
-                title,
-                author,
-                publisher
-            }
-        })
+        if (!title && !author && !publisher) {
+            throw new BadRequestException({
+                message: "Должно быть заполнено хотя-бы одно поле"
+            })
+        }
+        try {
+            return this.prismaService.book.create({
+                data: {
+                    title,
+                    author,
+                    publisher
+                }
+            })
+        } catch (error) {
+            console.error(error);
+            throw new ConflictException({
+                message: `Произошла ошибка при создании: ${error}`,
+            })
+        }
     }
 }
